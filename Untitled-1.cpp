1ffd7f1d9e6732c699d363861943e106c5dae265
Tihendii <47138445+Tihendii@users.noreply.github.com>
2023-04-03 13:02:30 +0300

    Add files via upload

--- Untitled-1.cpp
+++ Untitled-1.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <SDL2/SDL.h>
 #include <string>
+#include <list>
 #include "map.h"
 using namespace std;
 const int SCREEN_WIDTH = 1920;
@@ -9,7 +10,80 @@ const int SCREEN_HEIGHT = 1080;
  
 SDL_Window *window = nullptr;
 SDL_Renderer *renderer = nullptr;
+class Objects{
+    public:
+        float x,y,w,h,speed;int dir;
+        bool life,isMove,isSelect,onGround;
+        enum {left,right,up,down,jump,stay} state;
+        Objects(SDL_Texture *image, float X, float Y, int W, int H) {
+			x = X; y = Y; w = W; h = H;
+			life = true; isMove = false;
+		}
 
+
+};
+class Player :public Objects {
+public:
+	bool isShoot;
+	enum { left, right, up, down, stay } state;
+
+	Player(SDL_Texture image, float X, float Y, int W, int H) :Objects(image, X, Y, W, H) {
+
+	void control() {
+		if (Keyboard::isKeyPressed) {
+			if (Keyboard::isKeyPressed(Keyboard::Left)) {
+				state = left; speed = speed;
+			}
+			if (Keyboard::isKeyPressed(Keyboard::Right)) {
+				state = right; speed = speed;
+			}
+
+			if (Keyboard::isKeyPressed(Keyboard::Up)) {
+				state = up; speed = speed;
+			}
+
+			if (Keyboard::isKeyPressed(Keyboard::Down)) {
+				state = down; speed = speed;
+			}
+		}
+	}
+
+	void checkCollisionWithMap(float Dx, float Dy)//ô öèÿ ïðîâåðêè ñòîëêíîâåíèé ñ êàðòîé
+	{
+		/**for ()//ïðîõîäèìñÿ ïî ýëåìåíòàì êàðòû
+			for ()
+			{
+				if (Map[i][j] == '1')
+				{
+					if (Dy > 0) {}//ïî Y âíèç=>èäåì â ïîë(ñòîèì íà ìåñòå).
+					if (Dy < 0) {}//ñòîëêíîâåíèå ñ âåðõíèìè êðàÿìè êàðòû
+					if (Dx > 0) {}//ñ ïðàâûì êðàåì êàðòû
+					if (Dx < 0) {}// ñ ëåâûì êðàåì êàðòû
+				}
+			}
+	*/
+	};
+	void update(float time)
+	{
+		control();//ôóíêöèÿ óïðàâëåíèÿ ïåðñîíàæåì
+		switch (state)//òóò äåëàþòñÿ ðàçëè÷íûå äåéñòâèÿ â çàâèñèìîñòè îò ñîñòîÿíèÿ
+		{
+		case right:dx = speed; break;//ñîñòîÿíèå èäòè âïðàâî
+		case left:dx = -speed; break;//ñîñòîÿíèå èäòè âëåâî
+		case up: dy = speed; break;
+		case down: dy = -speed; break;
+		case stay: break;		
+		}
+		x += dx * time;
+		checkCollisionWithMap(dx, 0);//îáðàáàòûâàåì ñòîëêíîâåíèå ïî Õ
+		y += dy * time;
+		checkCollisionWithMap(0, dy);//îáðàáàòûâàåì ñòîëêíîâåíèå ïî Y
+		sprite.setPosition(x + w / 2, y + h / 2); //çàäàåì ïîçèöèþ ñïðàéòà â ìåñòî åãî öåíòðà
+		if (health <= 0) { life = false; }
+		if (!isMove) { speed = 0; }
+		//if (life) { setPlayerCoordinateForView(x, y); } ìîæíî äîáàâèòü, ÷òîá êàìåðà íå âñåãäà áûëà íàä ïåðñîíàæåì
+	}
+};
 void renderTextureA(SDL_Texture *tex, SDL_Renderer *ren, SDL_Rect dst,
         SDL_Rect *clip = nullptr)
 {
@@ -58,14 +132,20 @@ void ApplySurface(int x, int y, SDL_Texture *tex, SDL_Renderer *rend){
    SDL_QueryTexture(tex, NULL, NULL, &pos.w, &pos.h);
    SDL_RenderCopy(rend, tex, NULL, &pos);
 }
+void falling(){
+    for 
+
+
+}
 
 int main(int argc, char * argv[])
 {
-	
+// list<allObj[]
 int iW = 85, iH = 150;
+
 int x = SCREEN_WIDTH / 2 - iW / 2;
 int y = SCREEN_HEIGHT / 2 - iH / 2;
-
+Player player(x,y);
 //÷àñòè 
 SDL_Rect clips[6];
 for (int i = 0; i < 6; ++i){
@@ -127,6 +207,7 @@ while (!quit)
 		{
 			quit = true;
 		}
+        // falling();
 		if (e.type == SDL_KEYDOWN)
 		{
 			 switch (e.key.keysym.sym){
@@ -175,10 +256,10 @@ while (!quit)
 
     
   
-     SDL_DestroyTexture(background);
-   SDL_DestroyTexture(image);
-   SDL_DestroyRenderer(renderer);
-   SDL_DestroyWindow(window);
-   SDL_Quit();
+    SDL_DestroyTexture(background);
+    SDL_DestroyTexture(image);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 	return 0;
 }
