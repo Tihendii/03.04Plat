6557d0f7f3df8e26bd4783b226e606ec5aab8652
Tihendii <47138445+Tihendii@users.noreply.github.com>
2023-04-03 11:47:51 +0300

    Add files via upload

---
+++ map.h
@@ -0,0 +1,18 @@
+#include <string>
+const int HEIGHT_MAP = 13;
+const int WIDTH_MAP = 21;
+std::string TileMap[HEIGHT_MAP] = {
+"                   ",
+"                   ",
+"                   ",
+"                   ",
+"                   ",
+"                   ",
+"                   ",
+"                   ",
+"                   ",
+"                   ",
+"                   ",
+"                   ",
+"                   ",
+};
